9234a3f06d8fc2867b853949cd00dc0c3326bacd
Your Name <you@example.com>
2022-10-27 12:52:45 +0200

    TODO list title and size added

--- src/controller/index.controller.js
+++ src/controller/index.controller.js
@@ -1,21 +1,24 @@
-const path = require('path');
-
 var todoList = [];
 
 const controller = {
     index: (req, res)=>{
-        res.render('index', {
+        var todoListSize = todoList.length;
+        var model = {
             title: 'TODO app',
+            listTitle: 'TODO list',
+            todoListSize: todoListSize,
             todoList: todoList
-        });
+        }
+
+        res.render('index', model);
     },
     update: (req, res)=>{
-        var value = req.body.text;
+        var value = req.body.todo_text;
         if(!todoList.includes(value)){
             todoList.push(value);
         };
 
-        return res.redirect('/');
+        return res.redirect(req.originalUrl);
     }
 }
 
